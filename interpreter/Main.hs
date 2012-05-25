3dc31cc075fe73a43b90c30d7cd612cec7bc163a
Henning Basold <h.basold@googlemail.com>
2012-05-25 13:56:26 +0000

    Made main more readable

--- interpreter/Main.hs
+++ interpreter/Main.hs
@@ -16,12 +16,76 @@ import Lang.LAMA.Identifier
 import Data.Map as Map
 import Data.Graph.Inductive.PatriciaTree
 import qualified Data.Graph.Inductive.Tree as GTree
-import Control.Monad (void)
+import Control.Monad (void, when, MonadPlus(..))
+import Control.Monad.Trans.Maybe
+import Control.Monad.IO.Class
 
 import Lang.LAMA.Parse
 import Lang.LAMA.Dependencies
+import Lang.LAMA.Typing.TypedStructure
+
+main :: IO ()
+main = do args <- getArgs
+          case args of
+            [] -> BL.hGetContents stdin >>= void . runMaybeT . run 2 "stdin"
+            "-v":v:fs -> mapM_ (runFile $ read v) fs
+            fs -> mapM_ (runFile 0) fs
 
 type Verbosity = Int
+
+whenV :: Monad m => Verbosity -> Verbosity -> m () -> m ()
+whenV atLeast v = when (v >= atLeast)
+
+putStrV :: Verbosity -> Verbosity -> String -> IO ()
+putStrV atLeast v s = whenV atLeast v (putStrLn s)
+
+runFile :: Verbosity -> FilePath -> IO ()
+runFile v f = putStrLn f >> BL.readFile f >>= runMaybeT' . run v f
+  where
+    runMaybeT' :: Functor m => MaybeT m () -> m ()
+    runMaybeT' = void . runMaybeT
+
+run :: Verbosity -> FilePath -> BL.ByteString -> MaybeT IO ()
+run v f inp = do
+  concTree <- checkErrors $ parseLAMA inp
+  liftIO $ putStrV 2 v $ show concTree
+  deps <- checkDeps $ mkDeps concTree
+  liftIO $ whenV 1 v (showDeps f deps)
+
+checkErrors :: Either Error Program -> MaybeT IO Program
+checkErrors r = case r of
+  Left (ParseError pe) -> printAndFail $ "Parse failed:\n" ++ pe
+  Left (StaticError se) -> printAndFail $ "Conversion failed:\n" ++ se
+  Left (TypeError te) -> printAndFail $ "Type check failed:\n" ++ te
+  Right concTree -> return concTree
+
+checkDeps :: Either String ProgDeps -> MaybeT IO ProgDeps
+checkDeps d = case d of
+  Left err -> printAndFail $ "Dependency error:\n" ++ err
+  Right deps -> return deps
+
+printAndFail :: String -> MaybeT IO a
+printAndFail e = liftIO (putStrLn e) >> mzero
+
+showDeps :: FilePath -> ProgDeps -> IO ()
+showDeps f deps = do
+    let progName = takeBaseName f
+    -- dependency graph for program
+    void $ runGraphviz (defaultVis $ progDepsFlow deps) Svg (progName ++ ".svg")
+    -- dependency graphs for nodes
+    forM_ (Map.toList $ progDepsNodes deps) (uncurry $ showNodes progName)
+  where
+    showNodes path n nDeps = do
+      let thisPath = path ++ "_" ++ identString n
+      void $ runGraphviz (defaultVis $ nodeDepsFlow nDeps) Svg (thisPath ++ ".svg")
+      forM_ (Map.toList $ nodeDepsNodes nDeps) (uncurry $ showNodes thisPath)
+
+defaultVis :: (G.Graph gr, Labellable nl) => gr (nl, a) el -> DotGraph G.Node
+defaultVis = graphToDot params
+  where params = nonClusteredParams {
+          globalAttributes = [GraphAttrs [RankDir FromTop]],
+          fmtNode = \(_, (v, _)) -> [Label $ toLabelValue v]
+        }
 
 instance forall a b. (Show a, Show b) => Show (Gr a b) where
   show g =
@@ -39,49 +103,3 @@ instance Labellable Var where
       prettyVar ((Id x _), u, m) = BS.unpack x ++ "(" ++ show u ++ prettyMode m ++ ")"
       prettyMode GlobalMode = ""
       prettyMode (LocationMode (Id l _)) = " in " ++ BS.unpack l
-
-putStrV :: Verbosity -> Verbosity -> String -> IO ()
-putStrV whenV v s = if v >= whenV then putStrLn s else return ()
-
-runFile :: Verbosity -> FilePath -> IO ()
-runFile v f = putStrLn f >> BL.readFile f >>= run v f
-
-run :: Verbosity -> FilePath -> BL.ByteString -> IO ()
-run v f inp = case parseLAMA inp of
-  Left (ParseError pe) -> do
-    putStrLn "Parse failed..."
-    putStrLn pe
-  Left (StaticError se) -> do
-    putStrLn $ "Conversion failed:"
-    putStrLn se
-  Left (TypeError te) -> do
-    putStrLn "Type check failed:"
-    putStrLn te
-  Right concTree -> case mkDeps concTree of
-    Left err -> putStrLn "Dependency error: " >> putStrLn err
-    Right deps -> do
-      putStrV 2 v $ show concTree
-      let progName = takeBaseName f
-      -- dependency graph for program
-      void $ runGraphviz (defaultVis $ progDepsFlow deps) Svg (progName ++ ".svg")
-      -- dependency graphs for nodes
-      forM_ (Map.toList $ progDepsNodes deps) (uncurry $ showNodes progName)
-  where
-    showNodes path n nDeps = do
-      let thisPath = path ++ "_" ++ identString n
-      void $ runGraphviz (defaultVis $ nodeDepsFlow nDeps) Svg (thisPath ++ ".svg")
-      forM_ (Map.toList $ nodeDepsNodes nDeps) (uncurry $ showNodes thisPath)
-
-defaultVis :: (G.Graph gr, Labellable nl) => gr (nl, a) el -> DotGraph G.Node
-defaultVis = graphToDot params
-  where params = nonClusteredParams {
-          globalAttributes = [GraphAttrs [RankDir FromTop]],
-          fmtNode = \(_, (v, _)) -> [Label $ toLabelValue v]
-        }
-
-main :: IO ()
-main = do args <- getArgs
-          case args of
-            [] -> BL.hGetContents stdin >>= run 2 "stdin"
-            "-v":v:fs -> mapM_ (runFile $ read v) fs
-            fs -> mapM_ (runFile 0) fs
