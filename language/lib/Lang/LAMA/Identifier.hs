eb2dd22963fa93d8cb16439f168d0edac0d9eaf9
Henning Basold <h.basold@googlemail.com>
2012-06-26 08:12:52 +0000

    Made identifiers an instance of IsString to build them from strings

--- language/lib/Lang/LAMA/Identifier.hs
+++ language/lib/Lang/LAMA/Identifier.hs
@@ -1,8 +1,9 @@
 module Lang.LAMA.Identifier where
 
 import qualified Data.ByteString.Char8 as BS
+import Data.String (IsString(..))
 
-class (Eq i, Ord i, Show i) => Ident i where
+class (Eq i, Ord i, Show i, IsString i) => Ident i where
   identBS :: i -> BS.ByteString
   identPretty :: i -> String
 
@@ -18,11 +19,17 @@ instance Eq PosIdent where
 instance Ord PosIdent where
   compare (PosIdent s1 _) (PosIdent s2 _) = compare s1 s2
 
+instance IsString PosIdent where
+  fromString s = PosIdent (BS.pack s) (0,0)
+
 instance Ident PosIdent where
   identBS (PosIdent s _) = s
   identPretty (PosIdent x (l, c)) = show x ++ " in line " ++ show l ++ " at column " ++ show c
 
 newtype SimpIdent = SimpIdent BS.ByteString deriving (Eq, Ord, Show)
+
+instance IsString SimpIdent where
+  fromString s = SimpIdent $ BS.pack s
 
 instance Ident SimpIdent where
   identBS (SimpIdent s) = s
