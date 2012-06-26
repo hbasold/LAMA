ebd67b7a5f27195db1aa6c7a1f6770aa0d833c92
Henning Basold <h.basold@googlemail.com>
2012-06-26 09:50:47 +0000

    Parse command line options

--- scade2lama/Main.hs
+++ scade2lama/Main.hs
@@ -1,10 +1,12 @@
 module Main (main) where
 
-import System.IO (stdin, hGetContents)
+import System.IO (stdin, stdout, stderr, hGetContents, hPutStr, hPutStrLn)
 import System.Environment (getArgs)
+import System.Console.GetOpt
+import System.Exit (exitSuccess)
 
 import Control.Monad.Trans.Maybe
-import Control.Monad (void, when, MonadPlus(..))
+import Control.Monad (when, MonadPlus(..))
 import Control.Monad.IO.Class (liftIO)
 
 import Language.Scade.Lexer (alexScanTokens)
@@ -15,38 +17,85 @@ import Transform
 import Lang.LAMA.Pretty
 import qualified Lang.LAMA.Structure.SimpIdentUntyped as L
 
+data Options = Options
+  { optInput :: FilePath
+  , optOutput :: FilePath
+  , optDebug :: Bool
+  , optDumpScade :: Bool
+  , optShowHelp :: Bool
+  }
+
+defaultOptions :: Options
+defaultOptions = Options
+  { optInput              = "-"
+  , optOutput             = "-"
+  , optDebug              = False
+  , optDumpScade          = False
+  , optShowHelp           = False
+  }
+
+resolveDebug :: Maybe String -> Options -> Options
+resolveDebug Nothing opts = opts {optDebug = True}
+resolveDebug (Just "dump-scade") opts = opts {optDumpScade = True}
+resolveDebug _ opts = opts
+
+options :: [OptDescr (Options -> Options)]
+options =
+  [ Option ['i']     []
+      (ReqArg (\f opts -> opts {optInput = f}) "FILE")
+      "input FILE or stdin"
+    , Option ['f'] ["file"] (ReqArg (\f opts -> opts {optOutput = f}) "FILE")
+      "output FILE or stdout"
+    , Option ['d'] ["debug"]
+      (OptArg resolveDebug "WHAT")
+      "Debug something; for more specific debugging use one of: [dump-scade]"
+    , Option ['h'] ["help"]
+      (NoArg  (\opts -> opts {optShowHelp = True}))
+      "Show this help"
+  ]
+
+interpreterOpts :: [String] -> IO Options
+interpreterOpts argv =
+  case getOpt (ReturnInOrder (\f opts -> opts {optInput = f})) options argv of
+    (o,[],[]) ->
+      let opts = foldl (flip id) defaultOptions o
+      in return opts
+    (_, (_:_), []) -> ioError $ userError "Unused free option -- should not happen"
+    (_,_,errs) -> ioError (userError (concat errs ++ usage))
+
+usage :: String
+usage = usageInfo header options
+  where header = "Usage: pcfi [OPTION...] files..."
+
 main :: IO ()
-main = do args <- getArgs
-          case args of
-            [] -> hGetContents stdin >>= void . runMaybeT . run 2 "stdin"
-            "-v":v:fs -> mapM_ (runFile (read v)) fs
-            fs -> mapM_ (runFile 0) fs
+main = do
+  argv <- getArgs
+  opts <- interpreterOpts argv
+  when (optShowHelp opts) $ do
+    putStr usage
+    exitSuccess
+  r <- case (optInput opts) of
+    "-" -> hGetContents stdin >>= runMaybeT . run opts "stdin"
+    f -> runFile opts f
+  case r of
+    Just s -> case (optOutput opts) of
+      "-" -> hPutStr stdout s
+      f -> writeFile f s
+    Nothing -> return ()
 
-type Verbosity = Int
+runFile :: Options -> FilePath -> IO (Maybe String)
+runFile opts f = putStrLn f >> readFile f >>= runMaybeT . run opts f
 
-whenV :: Monad m => Verbosity -> Verbosity -> m () -> m ()
-whenV atLeast v = when (v >= atLeast)
-
-putStrV :: Verbosity -> Verbosity -> String -> IO ()
-putStrV atLeast v s = whenV atLeast v (putStrLn s)
-
-runFile :: Verbosity -> FilePath -> IO ()
-runFile v f = putStrLn f >> readFile f >>= runMaybeT' . run v f
-  where
-    runMaybeT' :: Functor m => MaybeT m () -> m ()
-    runMaybeT' = void . runMaybeT
-
-run :: Verbosity -> FilePath -> String -> MaybeT IO ()
-run v f inp = do
+run :: Options -> FilePath -> String -> MaybeT IO String
+run opts f inp = do
   s <- checkScadeError $ scade $ alexScanTokens inp
-  liftIO $ putStrV 2 v $ show s
-  l <- checkTransformError $ transform s  
-  liftIO $ putStrV 1 v $ (f ++ " as LAMA:")
-  liftIO $ putStrV 1 v $ prettyLama l
+  liftIO $ when (optDumpScade opts) (putStrLn $ show s)
+  l <- checkTransformError $ transform s
+  return $ prettyLama l
 
 checkScadeError :: [Declaration] -> MaybeT IO [Declaration]
 checkScadeError = return
 
 checkTransformError :: Either String L.Program -> MaybeT IO L.Program
-checkTransformError (Left e) = (liftIO $ putStrLn $ "Transform error: \n" ++ e) >> mzero
+checkTransformError (Left e) = (liftIO $ hPutStrLn stderr $ "Transform error: \n" ++ e) >> mzero
 checkTransformError (Right p) = return p
