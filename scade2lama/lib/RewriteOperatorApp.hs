187adefefb3b548f26d1a983010828b8ad4f5958
Henning Basold <h.basold@googlemail.com>
2012-08-08 20:15:11 +0000

    Rewrite nested operator applications (not yet finished)

--- scade2lama/lib/RewriteOperatorApp.hs
+++ scade2lama/lib/RewriteOperatorApp.hs
@@ -17,9 +17,76 @@
 
 module RewriteOperatorApp where
 
+import Data.Data
+import Data.Generics.Schemes (everywhereM)
+import Data.Generics.Aliases (mkM)
+
+import Control.Monad (liftM)
+import Control.Monad.State (StateT(..), modify)
+
 import Language.Scade.Syntax as S
 
 import VarGen
 
 rewrite :: MonadVarGen m => [Declaration] -> m [Declaration]
-rewrite = return
+rewrite = everywhereM (mkM rewriteDataDef)
+
+-- | DataDef is the only place where we can put data flow, so
+-- we start here.
+rewriteDataDef :: MonadVarGen m => DataDef -> m DataDef
+rewriteDataDef def =
+  do (eqs', (extraEqs, extraVars)) <-
+       (flip runStateT ([], [])) $ mapM rewriteEquation $ dataEquations def
+     return $ def { dataLocals = dataLocals def ++ extraVars
+                  , dataEquations = eqs' ++ extraEqs }
+
+type RewrT = StateT ([Equation], [VarDecl])
+
+rewriteEquation :: MonadVarGen m => Equation -> RewrT m Equation
+rewriteEquation (SimpleEquation lhs expr) = liftM (SimpleEquation lhs) $ rewriteExprTop expr
+rewriteEquation (AssertEquation assertType assertName expr) =
+  liftM (AssertEquation assertType assertName) $ rewriteExprAll expr
+rewriteEquation (EmitEquation (EmissionBody sigs (Just expr))) =
+  liftM (EmitEquation . EmissionBody sigs . Just) $ rewriteExprAll expr
+rewriteEquation (StateEquation sm returns retAll) =
+  rewriteStateMachine sm >>= \sm' -> return $ StateEquation sm' returns retAll
+rewriteEquation eq = return eq
+
+-- | Avoid unrolling on top level
+rewriteExprTop :: MonadVarGen m => Expr -> RewrT m Expr
+rewriteExprTop (ApplyExpr op es) = liftM (ApplyExpr op) $ rewriteExprAll es
+rewriteExprTop e = rewriteExprAll e
+
+rewriteExpr :: MonadVarGen m => Expr -> RewrT m Expr
+rewriteExpr app@(ApplyExpr _ _) =
+  do appRes <- newVar "opresult"
+     -- FIXME: we need the type of the operator here
+     modify $ \(eqs, vs) -> ((SimpleEquation [Named appRes] app) : eqs, vs)
+     return . IdExpr $ Path [appRes]
+rewriteExpr e = return e
+
+rewriteExprAll :: (MonadVarGen m, Data a) => a -> RewrT m a
+rewriteExprAll = everywhereM $ mkM rewriteExpr
+
+rewriteStateMachine :: MonadVarGen m => StateMachine -> RewrT m StateMachine
+rewriteStateMachine (StateMachine smName states) =
+  liftM (StateMachine smName) $ mapM rewriteState states
+
+-- | Only rewrites transitions. The flow is already handled
+-- earlier.
+rewriteState :: MonadVarGen m => State -> RewrT m State
+rewriteState s =
+  do unless' <- mapM rewriteTransition $ stateUnless s
+     until' <- mapM rewriteTransition $ stateUntil s
+     return $ s { stateUnless = unless'
+                , stateUntil = until' }
+
+-- | Fixme: pull out from ActionEmission and ConditionalFork
+rewriteTransition :: MonadVarGen m => Transition -> RewrT m Transition
+rewriteTransition (Transition cond actions fork) =
+  do cond' <- rewriteExprAll cond
+     fork' <- rewriteFork fork
+     return $ Transition cond' actions fork'
+
+rewriteFork :: MonadVarGen m => Fork -> RewrT m Fork
+rewriteFork f = return f
