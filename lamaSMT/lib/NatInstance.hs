80bc0f6dac06994bd075409baff35a634c3563e5
Henning Basold <h.basold@googlemail.com>
2014-02-24 14:24:12 +0100

    Fix compilation: adapted Natural to new type representation

--- lamaSMT/lib/NatInstance.hs
+++ lamaSMT/lib/NatInstance.hs
@@ -9,68 +9,116 @@ module NatInstance where
 
 import Data.Natural
 import Data.Unit
+import Data.Fix
 import Language.SMTLib2.Internals
 import Language.SMTLib2
-import Language.SMTLib2.Functions
-import qualified Data.AttoLisp as L
+import Language.SMTLib2.Internals.Operators
 
 import Data.Typeable
-
-import Control.Monad (liftM)
 
 deriving instance Typeable Natural
 
 data NatImplementation =
-  NatType
-  | NatInt
-  deriving (Eq, Typeable)
-
-instance SMTType Natural where
-  type SMTAnnotation Natural = NatImplementation
-
-  getSort _ NatType = L.Symbol "Nat"
-  getSort _ NatInt = getSort (undefined :: Integer) unit
-
-  getSortBase _ = undefined
-
-  declareType u NatType =
-    declareType' (DeclaredType u NatType) decl
-    where
-      decl = declareDatatypes [] [
-        ("Nat",
-         [("zero", []),
-          ("succ", [("pred", L.Symbol "Nat")])
-         ])]
-  declareType _ NatInt = declareType (undefined :: Integer) unit
-
-instance SMTValue Natural where
-  unmangle (L.Symbol "zero") NatType = Just $ fromInteger 0
-  unmangle (L.List [L.Symbol "succ", r]) NatType = fmap (+1) $ unmangle r NatType
-  unmangle x NatInt = fmap fromInteger $ unmangle x unit
-  unmangle _ _ = Nothing
-
-  mangle (view -> Zero) NatType = L.Symbol "zero"
-  mangle (view -> Succ n) NatType = L.List [L.Symbol "succ", mangle n NatType]
-  mangle x NatInt = mangle (toInteger x) unit
-
--- | only correct with NatInt!!
-instance SMTArith Natural
--- | only correct with NatInt!!
-instance SMTOrd Natural where
-  (.<.) x y = App Lt (x,y)
-  (.<=.) x y = App Le (x,y)
-  (.>.) x y = App Gt (x,y)
-  (.>=.) x y = App Ge (x,y)
+  NatInt
+  | NatType
+  deriving (Eq, Ord, Show, Typeable)
 
 zero' :: SMTAnnotation Natural -> SMTExpr Natural
 zero' ann = constantAnn 0 ann
 
 succ' :: SMTAnnotation Natural -> SMTExpr Natural -> SMTExpr Natural
-succ' NatType e = SMTFun "succ" (extractAnnotation e) `app` e
+succ' NatType e = (SMTConstructor (Constructor "succ")) `app` e
 succ' NatInt e = app plus [e, (constantAnn 1 NatInt)]
 
+instance SMTType Natural where
+  type SMTAnnotation Natural = NatImplementation
+
+  -- getSort :: t -> SMTAnnotation t -> Sort
+  getSort _ NatInt = getSort (undefined :: Integer) unit
+  getSort _ NatType = Fix $ NamedSort "Nat" []
+
+  -- asDataType :: t -> SMTAnnotation t -> Maybe (String,TypeCollection)
+  asDataType _ NatInt = Nothing
+  asDataType _ NatType =
+    Just ("Nat",
+          TypeCollection { argCount = 0
+                         , dataTypes = [dtNat] })
+    where
+      dtNat =
+        DataType { dataTypeName = "Nat"
+                 , dataTypeConstructors = [zeroCon, succCon]
+                 , dataTypeGetUndefined =
+                   \_ f -> f (undefined :: Natural) NatType
+                 }
+      zeroCon =
+        Constr { conName = "zero"
+               , conFields = []
+               , construct =
+                 \[Just sort] _ f -> f [sort] (0 :: Natural) NatType
+               , conTest = \_ x -> case cast x of
+                   Just (view -> Zero) -> True
+                   _ -> False
+               }
+      succCon =
+        Constr { conName = "succ"
+               , conFields = [predField]
+               , construct =
+                 \_ [predAny] f -> case castAnyValue predAny of
+                   Just (pred, ann) -> f [ProxyArg (undefined :: Natural) ann]
+                                       (pred + 1 :: Natural) ann
+                   _ -> error $ "Casting succ argument failed"
+               , conTest = \_ x -> case cast x of
+                   Just (view -> Succ _) -> True
+                   _ -> False
+               }
+      predField = DataField { fieldName = "pred"
+                            , fieldSort = Fix (ArgumentSort 0)
+                            , fieldGet =
+                              \_ x f -> case cast x of
+                                Just (view -> Succ n, ann) -> f n ann
+                                _ -> error $ "Casting pred failed"
+                            }
+
+  -- asValueType :: t -> SMTAnnotation t -> (forall v. SMTValue v => v -> SMTAnnotation v -> r) -> Maybe r
+  asValueType x ann f = Just $ f x ann
+
+  -- getProxyArgs :: t -> SMTAnnotation t -> [ProxyArg]
+  getProxyArgs x ann = [ProxyArg x ann]
+
+  -- additionalConstraints :: t -> SMTAnnotation t -> SMTExpr t -> [SMTExpr Bool]
+  additionalConstraints _ NatInt x@(Var _ _)  = [x .>=. (zero' NatInt)]
+  additionalConstraints _ NatInt _            = []
+  additionalConstraints _ NatType _           = []
+
+  -- annotationFromSort :: t -> Sort -> SMTAnnotation t
+  annotationFromSort _ (Fix (NamedSort "Nat" [])) = NatType
+  annotationFromSort _ _                          = NatInt
+
+instance SMTValue Natural where
+  unmangle x NatInt = fmap fromInteger $ unmangle x unit
+  unmangle (ConstrValue "zero" [] _) NatType = Just $ fromInteger 0
+  unmangle (ConstrValue "succ" [n] _) NatType = fmap (+1) $ unmangle n NatType
+  unmangle _ _ = Nothing
+
+  mangle x NatInt = mangle (toInteger x) unit
+  mangle (view -> Zero) NatType = (ConstrValue "zero" [] Nothing)
+  mangle (view -> Succ n) NatType =
+    (ConstrValue "succ" [mangle n NatType] Nothing)
+
+-- | only correct with NatInt!!
+instance SMTArith Natural
+-- | only correct with NatInt!!
+instance SMTOrd Natural where
+  (.<.) x y = App (SMTOrd Lt) (x,y)
+  (.<=.) x y = App (SMTOrd Le) (x,y)
+  (.>.) x y = App (SMTOrd Gt) (x,y)
+  (.>=.) x y = App (SMTOrd Ge) (x,y)
+
+
+{-
 natVar :: MonadSMT m => SMTAnnotation Natural -> m (SMTExpr Natural)
 natVar NatType = liftSMT $ varAnn NatType
 natVar NatInt = liftSMT $ varAnn NatInt >>=
                 \x -> assert (x .>=. (zero' NatInt)) >>
                       return x
+-}
